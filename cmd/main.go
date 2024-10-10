84ec4d5bb3212cb0a8708e896d3870fb892b9021
Mover-R <denis.tugow2014@gmail.com>
2024-10-10 23:49:42 +0300

    Changed GetType in main

--- cmd/main.go
+++ cmd/main.go
@@ -14,7 +14,7 @@ func main() {
 	s.Set("0", "mamba")
 	s.Set("1", "22")
 	s.Set("2", "22.2")
-	fmt.Println(*s.Get("0"), storage.Get_type(*s.Get("0")))
-	fmt.Println(*s.Get("1"), storage.Get_type(*s.Get("1")))
-	fmt.Println(*s.Get("2"), storage.Get_type(*s.Get("2")))
+	fmt.Println(*s.Get("0"), storage.GetType(*s.Get("0")))
+	fmt.Println(*s.Get("1"), storage.GetType(*s.Get("1")))
+	fmt.Println(*s.Get("2"), storage.GetType(*s.Get("2")))
 }
