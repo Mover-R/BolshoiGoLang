e951ca1a1ab2c36fab875aa64607bae64c58db23
Mover-R <denis.tugow2014@gmail.com>
2024-10-11 11:09:07 +0300

    Tests done without formating

--- internal/pkg/storage/storage_test.go
+++ internal/pkg/storage/storage_test.go
@@ -24,3 +24,32 @@ func TestGetType(t *testing.T) {
 		}
 	}
 }
+
+func TestSetGet(t *testing.T) {
+	fmt.Println("Tests for Set() Get()")
+	s, err := NewStorage()
+	if err != nil {
+		t.Fatal("Storage didnt created")
+	}
+	tests := []struct {
+		in  string
+		out string
+	}{
+		{"key1", "value1"},
+		{"key2", "value2"},
+		{"key3", "value3"},
+		{"key4", ""},
+	}
+	for _, test := range tests {
+		s.Set(test.in, test.out)
+	}
+	for _, test := range tests {
+		res := s.Get(test.in)
+		if res == nil {
+			t.Fatal("No such key")
+		}
+		if (*res) != test.out {
+			t.Errorf("Get(%q) = %q != Expected %q", test.in, *res, test.out)
+		}
+	}
+}
